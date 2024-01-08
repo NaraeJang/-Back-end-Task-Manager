4db1c6c65ac538d03818c5fd5c50ebaa3e38479a
NaraeJang <j.naaaarae@gmail.com>
2024-01-08 14:44:25 -0500

    Connect to DB, Remove some of deprecated warning

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectionString =
-  "mongodb+srv://narae:narae1234@cluster0.66ch5yj.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
+  "mongodb+srv://narae:narae1234@cluster0.66ch5yj.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
-  .connect(connectionString)
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then((result) => console.log(`CONNECTED TO THE DB...`))
   .catch((err) => {
     console.log(err);
