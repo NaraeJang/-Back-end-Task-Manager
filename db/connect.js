025e19297712bd4eb729146bfbbe2c409123c0f3
NaraeJang <j.naaaarae@gmail.com>
2024-01-08 14:48:40 -0500

    refactor Code: in order to connect with database first, and then connect with the server

--- db/connect.js
+++ db/connect.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://narae:narae1234@cluster0.66ch5yj.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then((result) => console.log(`CONNECTED TO THE DB...`))
-  .catch((err) => {
-    console.log(err);
   });
+};
+
+module.exports = connectDB;
